94d13905d5aa264a088239b999fe1e313f0108a4
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2018-03-27 17:50:56 +0300

    test. Test init background

--- src/display.cpp
+++ src/display.cpp
@@ -1,6 +1,12 @@
 
 #include "display.h"
 #include "frame_keeper.h"
+#include <iostream>
+
+struct Frame{
+    uint8_t data[3][400*400*3];
+    uint64_t linesize[3];
+} _frame;
 
 void Display::runDisplay()
 {
@@ -34,6 +40,49 @@ std::string Display::init(int pos_x, int pos_y, int _width, int _height)
     if (!renderer) {
         return std::string("SDL: could not create renderer - exiting");
     }
+
+
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_YV12,
+                            SDL_TEXTUREACCESS_TARGET, 400, 400);
+    _frame.linesize[0] = 400;
+    _frame.linesize[1] = 200;
+    _frame.linesize[2] = 200;
+
+    /* Y */
+    int x,y;
+    int i = 1;
+    for (y = 0; y < 400; y++)
+        for (x = 0; x < 400; x++)
+            _frame.data[0][y * _frame.linesize[0] + x] = x + y + i * 3;
+
+    /* Cb and Cr */
+    for (y = 0; y < 400 / 2; y++) {
+        for (x = 0; x < 400 / 2; x++) {
+            _frame.data[1][y * _frame.linesize[1] + x] = 128 + y + i * 2;
+            _frame.data[2][y * _frame.linesize[2] + x] = 64 + x + i * 5;
+        }
+    }
+
+    std::cout << "init frame->linesize[0]: " << _frame.linesize[0] << std::endl;
+    std::cout << "init frame->linesize[1]: " << _frame.linesize[1] << std::endl;
+    std::cout << "init frame->linesize[2]: " << _frame.linesize[2] << std::endl;
+
+    SDL_UpdateYUVTexture(
+        texture,
+        NULL,
+        _frame.data[0], //vp->yPlane,
+        _frame.linesize[0],
+        _frame.data[1], //vp->yPlane,
+        _frame.linesize[1],
+        _frame.data[2], //vp->yPlane,
+        _frame.linesize[2]
+    );
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    SDL_DestroyTexture(texture);
+
     return std::string{};
 }
 
@@ -50,10 +99,6 @@ void Display::stopDisplay()
     SDL_DestroyRenderer(renderer);
 }
 
-struct Frame{
-    uint8_t data[3][400*400*3];
-    uint64_t linesize[3];
-} _frame;
 
 void Display::displayFrame(AVFrame *frame)
 {
